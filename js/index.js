75ac27f1c7b9147617996d79b619d0d0d84e56ef
Zahidul Haque <jahidulhaque767@gmail.com>
2023-02-11 19:03:56 +0600

    complete navbar responsive

--- js/index.js
+++ js/index.js
@@ -24,3 +24,31 @@ var swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// Humbarger button
+const navSlide = () => {
+  const burger = document.querySelector(".fa-bars");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  //Toggle Nav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+
+    //burger animation
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
